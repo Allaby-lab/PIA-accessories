afd4d6b8aae9e88f027d6972dfda653df837b339
Allaby Lab <r.cribdon@warwick.ac.uk>
2020-05-11 13:51:00 +0100

    Minor formatting change

--- Merge_Summary_Basics.pl
+++ Merge_Summary_Basics.pl
@@ -2,27 +2,60 @@
 
 use strict;
 use warnings;
+use Getopt::Std;
 
 ######################################################
 ############## Merge_Summary_Basics.pl ###############
 ############## Becky Cribdon, UoW 2013 ###############
 ######################################################
-############## Version 0.2, 2019-11-26 ###############
+############## Version 0.5, 2020-05-11 ###############
 ######################################################
 
 # This script combines Summary_Basic.txt files into one.
 # Run as follows:
 # >perl Merge_Summary_Basics.pl [at least one summary basic]
 
+my %options = ();
+getopts('ho:', \%options);
 
-print "\n\n##### Merging summary basics #####\n\n";
+if ($options{h}) { # If the help option is called, print the help text and exit.
+    print "Usage: perl Merge_Summary_Basics.pl [-ho] [at least one Summary Basic]
+
+Option	Description			Explanation
+-h	Help				Print this message.
+-o	Output file name                Output file name. Defaults to the first Summary Basic with '_merged.txt'.
+
+
+Other arguments	    Description
+[at least one Summary Basic]    Can be pruned or not.
+
+";
+	exit;
+}
+
+my @output_filename = split ('_', $ARGV[0]); # Start with the first input file name.
+my $output_filename = join ('_', @output_filename[0..1], $output_filename[3], 'merged', $output_filename[-1]); # Trim out replicate-specific parts and add 'merged'.
+if ($options{o}) { # If the output name option is called, overwrite the default with the option input.
+    $output_filename = $options{o};
+    print "\n-o: Using output base name '$output_filename'\n";
+}
+
+
 
 # Read in the summary basics and add data to a single hash
 #---------------------------------------------------------
 my @summary_basic_filenames = @ARGV; # Any other inputs are summary basics.
 my %taxa_and_hits = ();
+my $last_sumbasic_filename = '';
+
+print "\nMerging:\n\n";
 
 foreach my $sumbasic_filename (@summary_basic_filenames) {
+        
+        print "\t$sumbasic_filename\n";
+        
+        if ($sumbasic_filename eq $last_sumbasic_filename) { print "\n\n### WARNING: Two consecutive summary basics have the same file name. Did you mean to merge them?\n\n"; }
+        
         open (my $sumbasic_filehandle, $sumbasic_filename) or die "Could not open summary basic file $sumbasic_filename: $!\n";
         foreach my $line (readline($sumbasic_filehandle)) {
             
@@ -40,16 +73,17 @@ foreach my $sumbasic_filename (@summary_basic_filenames) {
             }
         }
         close $sumbasic_filehandle;
+        
+        $last_sumbasic_filename = $sumbasic_filename;
 }
 
 
 # Print hash to a new file
 #-------------------------
-my $output_filename = 'Merged_summary_basic.txt';
 open( my $output_filehandle, '>', $output_filename) or die "Cannot open $output_filename for writing.\n$!\n";
 
 my $timestamp = localtime();
-print $output_filehandle "#Combined summary basic $timestamp\n"; # Print a header first.
+print $output_filehandle "#Combined Summary Basic $timestamp\n"; # Print a header first.
 
 foreach my $taxon (keys %taxa_and_hits) { # If there weren't any hits in any summary basic, this hash will be empty. But that shouldn't throw an error.
             print $output_filehandle "$taxon\t$taxa_and_hits{$taxon}\n";
